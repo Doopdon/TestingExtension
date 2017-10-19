b017fe14cd49c36e41b231742f5730d81adfdeb8
Doopdon <gannonhuiting@gmail.com>
2017-10-18 21:05:43 -0600

    added a testing page

---
+++ Test Page/script.js
@@ -0,0 +1,6 @@
+function update()
+{
+  document.getElementById("lbl1").innerHTML = document.getElementById("tx1").value;
+  document.getElementById("lbl2").innerHTML = document.getElementById("tx2").value;
+  document.getElementById("lbl3").innerHTML = document.getElementById("tx3").value;
+}
